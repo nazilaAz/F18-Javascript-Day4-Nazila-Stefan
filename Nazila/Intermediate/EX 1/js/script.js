94e71ef25f87d0a7adf194ebb425384ef9846e6e
Nazila <azabdaftar@outlook.com>
2023-02-07 14:58:11 +0100

    santa v1.0

---
+++ Nazila/Intermediate/EX 1/js/script.js
@@ -0,0 +1,13 @@
+function removeSanta() {
+    let santa = document.getElementsByClassName("santa");
+
+    for (let i = 0; i < santa.length; i++) {
+        santa[i].addEventListener("click", function() {
+            this.remove();
+        })
+    }
+}
+
+document.addEventListener("click", function() {
+    removeSanta();
+})
